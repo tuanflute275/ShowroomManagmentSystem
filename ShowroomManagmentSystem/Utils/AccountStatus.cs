c3ee6f1881e784b3a5d72d9452a6c9e5c9882abd
tuanflute275 <tuanflute275@gmail.com>
2024-12-05 17:49:41 +0700

    Refactor: add file Constans in folder Util

--- ShowroomManagmentSystem/Utils/AccountStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShowroomManagmentSystem.Utils
-{
-    public enum AccountStatus
-    {
-        Active      = 0,      // 0: Active    (Mở khóa)
-        Locked      = 1,      // 1: Locked    (Tạm khóa) 
-        Suspended   = 2       // 2: Suspended (Cấm)
-    }
-}
